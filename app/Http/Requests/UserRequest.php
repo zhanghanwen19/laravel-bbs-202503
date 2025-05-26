e110a4ddfb4a4452b698fa9d67bc62f07f4f4e2f
zhanghanwen19 <zhw597426798@gmail.com>
2025-05-26 16:28:11 +0800

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,6 +26,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[\x{4e00}-\x{9fff}\x{3400}-\x{4dbf}\x{3040}-\x{309f}\x{30a0}-\x{30ff}\x{31f0}-\x{31ff}\x{3005}a-zA-Z0-9_-]+$/u|unique:users,name,' . auth()->user()->id,
             'email' => 'required|email|unique:users,email,' . auth()->user()->id,
             'introduction' => 'max:80',
+            'avatar' => 'mimes:png,jpg,gif,jpeg|dimensions:min_width=208,min_height=208',
         ];
     }
 }
