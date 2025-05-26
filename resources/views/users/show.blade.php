899cbc0f644b654c6ce7664a39848bbce74ba1d5
zhanghanwen19 <zhw597426798@gmail.com>
2025-05-26 16:14:43 +0800

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,7 +16,7 @@
                     <p>{{ $user->introduction }}</p>
                     <hr>
                     <h5><strong>{{ __('Joined on') }}</strong></h5>
-                    <p>January 01 1901</p>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
