de21e426e2cf5eaca692b2083d5ab7a486918b2f
zhanghanwen19 <zhw597426798@gmail.com>
2025-05-26 16:13:49 +0800

    让更新用户名的时候支持中文日文

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -23,7 +23,7 @@ class UserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . auth()->user()->id,
+            'name' => 'required|between:3,25|regex:/^[\x{4e00}-\x{9fff}\x{3400}-\x{4dbf}\x{3040}-\x{309f}\x{30a0}-\x{30ff}\x{31f0}-\x{31ff}\x{3005}a-zA-Z0-9_-]+$/u|unique:users,name,' . auth()->user()->id,
             'email' => 'required|email|unique:users,email,' . auth()->user()->id,
             'introduction' => 'max:80',
         ];
