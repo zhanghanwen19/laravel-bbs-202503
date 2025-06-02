0c4367ac5f5c73b2dd1c0e5a85dc21e3b0bfd09d
zhanghanwen19 <zhw597426798@gmail.com>
2025-06-02 20:13:18 +0800

    发布话题的时候自动生成 slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -14,7 +14,16 @@ class TopicObserver
      */
     public function saving(Topic $topic): void
     {
+        // 过滤话题内容的特殊标签
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        // 生成摘要
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如果没有 slug，则使用标题生成 slug
+        // 我们按照查得的日本公司最常见的方式来处理 slug
+        if (!$topic->slug) {
+            $topic->slug = rawurlencode($topic->title);
+        }
     }
 }
