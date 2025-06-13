4e71a7f27730ab9319f94eb1ed9520164d3e0100
zhanghanwen19 <zhw597426798@gmail.com>
2025-06-13 15:35:12 +0900

    修改通知列表页面

--- resources/views/notifications/types/_topic_replied.blade.php
+++ resources/views/notifications/types/_topic_replied.blade.php
@@ -1,23 +1,29 @@
-<li class="d-flex @if (!$loop->last) border-bottom @endif">
-    <div>
+<li class="d-flex align-items-start py-3 @if (!$loop->last) border-bottom @endif">
+    <div class="me-3 flex-shrink-0">
         <a href="{{ route('users.show', $notification->data['user_id']) }}">
-            <img class="img-thumbnail mr-3" alt="{{ $notification->data['user_name'] }}" src="{{ $notification->data['user_avatar'] }}" style="width:48px;height:48px;" />
+            <img class="img-thumbnail" alt="{{ $notification->data['user_name'] }}"
+                 src="{{ $notification->data['user_avatar'] }}"
+                 style="width:48px; height:48px;" />
         </a>
     </div>
 
-    <div class="flex-grow-1 ms-2">
-        <div class="mt-0 mb-1 text-secondary">
-            <a class="text-decoration-none" href="{{ route('users.show', $notification->data['user_id']) }}">{{ $notification->data['user_name'] }}</a>
-            {{ __('replied to') }}
-            <a class="text-decoration-none" href="{{ $notification->data['topic_link'] }}">{{ $notification->data['topic_title'] }}</a>
-
-            {{-- 回复删除按钮 --}}
-            <span class="meta float-end" title="{{ $notification->created_at }}">
-        <i class="far fa-clock"></i>
-        {{ $notification->created_at->diffForHumans() }}
-      </span>
+    <div class="flex-grow-1">
+        <div class="d-flex justify-content-between mb-1 text-secondary small">
+            <div>
+                <a class="fw-bold text-decoration-none" href="{{ route('users.show', $notification->data['user_id']) }}">
+                    {{ $notification->data['user_name'] }}
+                </a>
+                {{ __('replied to') }}
+                <a class="text-decoration-none" href="{{ $notification->data['topic_link'] }}">
+                    {{ $notification->data['topic_title'] }}
+                </a>
+            </div>
+            <span class="text-muted me-2" title="{{ $notification->created_at }}">
+                <i class="far fa-clock me-1"></i>{{ $notification->created_at->diffForHumans() }}
+            </span>
         </div>
-        <div class="reply-content">
+
+        <div class="reply-content text-dark small">
             {!! $notification->data['reply_content'] !!}
         </div>
     </div>
