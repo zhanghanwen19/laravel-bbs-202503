f8fd0cfebd6294377a0f701d9393068c68075e37
zhanghanwen19 <zhw597426798@gmail.com>
2025-06-19 16:56:41 +0900

    修改内容

--- app/Console/Commands/CalculateActiveUser.php
+++ app/Console/Commands/CalculateActiveUser.php
@@ -5,28 +5,33 @@ namespace App\Console\Commands;
 use App\Models\User;
 use Illuminate\Console\Command;
 
-class SyncUserActiveAt extends Command
+#[AsScheduled('everyMinute')]
+class CalculateActiveUser extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'pandaria:sync-user-active-at';
+    protected $signature = 'pandaria:calculate-active-user';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = '将用户最后登录时间从 Redis 同步到数据库中';
+    protected $description = '生成活跃用户';
 
     /**
      * Execute the console command.
      */
     public function handle(User $user): void
     {
-        $user->syncUserActiveAt();
-        $this->info('同步成功！');
+        // 在命令行打印一行信息
+        $this->info("开始计算...");
+
+        $user->calculateAndCacheActiveUsers();
+
+        $this->info("成功生成！");
     }
 }
